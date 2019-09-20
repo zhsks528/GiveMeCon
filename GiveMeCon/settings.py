8cc7bf580dc76e8b9ddbf450dcb591fd2562861a
SeongMin Han <hsm950825@naver.com>
2019-09-20 23:05:58 +0900

    django extensions 추가 및 drf-yasg(API 문서화) 추가

--- GiveMeCon/settings.py
+++ GiveMeCon/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework', # djangorestframework
     'video', # video App
+    'drf_yasg', # API 문서화
 ]
 
 MIDDLEWARE = [
