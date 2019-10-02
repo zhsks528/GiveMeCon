58327bc87ec62a402fffdadfd319beb3f7b83455
SeongMin Han <hsm950825@naver.com>
2019-10-02 23:46:43 +0900

    Production Serializers 생성

---
+++ production/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Production
+
+
+class ProductionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Production
+        fields = "__all__"
+
