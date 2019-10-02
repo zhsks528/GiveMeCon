a785265d63c754800087ecf4fccd17b5499e4b2a
SeongMin Han <hsm950825@naver.com>
2019-10-02 23:38:55 +0900

    Production Model 생성

--- production/models.py
+++ production/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Production(models.Model):
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    thumnail = models.ImageField(null=True, blank=True)
+    author = models.CharField(max_length=255)
+    tag = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
