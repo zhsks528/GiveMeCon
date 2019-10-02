095baec9614f469da3398b8ec86ebc248bbf12d5
SeongMin Han <hsm950825@naver.com>
2019-10-02 23:47:11 +0900

    Production views 생성

--- production/views.py
+++ production/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Production
+from .serializers import ProductionSerializer
 
-# Create your views here.
+
+class ProductionViewSet(viewsets.ModelViewSet):
+    queryset = Production.objects.all()
+    serializer_class = ProductionSerializer
