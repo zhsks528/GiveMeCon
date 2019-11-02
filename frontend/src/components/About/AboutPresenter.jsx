5bada0964f87311b28a1b8bbb8065f17e8c01db8
SeongMin Han <hsm950825@naver.com>
2019-11-02 14:50:51 +0900

    Introduce 중앙정렬

--- frontend/src/components/About/AboutPresenter.jsx
+++ frontend/src/components/About/AboutPresenter.jsx
@@ -25,9 +25,9 @@ const TitleContainer = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  top: 60%;
+  top: 50%;
   left: 50%;
-  transform: translate(-60%, -50%);
+  transform: translate(-50%, -50%);
   text-align: center;
 `;
 
