23aa1952d8c9859fea8c0c2227bfa265f24ef0e9
SeongMin Han <hsm950825@naver.com>
2019-11-02 14:59:51 +0900

    중복 Router로 변경

--- frontend/src/components/Router/index.jsx
+++ frontend/src/components/Router/index.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-
+import Trend from "routes/Trend";
 import Production from "routes/Production";
 import Introduce from "routes/Introduce";
-import Trend from "routes/Trend";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Trend} />
-      <Route path="/production" exact component={Production} />
-      {/* <Route path="/production/music" exact component={Production} />
-      <Route path="/production/sports" exact component={Production} />
-      <Route path="/production/movie" exact component={Production} />
-      <Route path="/production/game" exact component={Production} /> */}
-      <Route path="/introduce" exact component={Introduce} />
-      <Redirect from="*" to="/" />
+      <Route path="/trend" component={Trend} />
+      <Route path="/production" component={Production} />
+      <Route exact path="/introduce" component={Introduce} />
+      <Redirect from="*" to="/trend" />
     </Switch>
   );
 }
--- frontend/src/routes/Production/ProductionContainer.jsx
+++ frontend/src/routes/Production/ProductionContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductionPresenter from "./ProductionPresenter";
 
-export default function ProductionContainer() {
-  return <ProductionPresenter />;
+export default function ProductionContainer({match}) {
+  
+  return <ProductionPresenter match={match}/>;
 }
