d3c30b7951d86dfcfcfd080d06d1f043069596dd
SeongMin Han <hsm950825@naver.com>
2019-11-03 01:13:41 +0900

    최근 업데이트 날짜 순서로 변경

--- production/models.py
+++ production/models.py
@@ -11,6 +11,9 @@ class Production(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+        
     def __str__(self):
         return self.title
 
