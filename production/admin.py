140c504840071d6ffa1dab04edabc931cdee3fbc
SeongMin Han <hsm950825@naver.com>
2019-11-02 14:49:49 +0900

    Production Music / Sports / Movies / Game admin 등록

--- production/admin.py
+++ production/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Production
+from .models import Production, ProduceMusic, ProduceSports, ProduceMovies, ProduceGame
 
 admin.site.register(Production)
+admin.site.register(ProduceMusic)
+admin.site.register(ProduceSports)
+admin.site.register(ProduceMovies)
+admin.site.register(ProduceGame)
