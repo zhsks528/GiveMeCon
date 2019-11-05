d4fa6bc994e4702c0117779a525f93d38e9b0401
Han Seong Min <hsm950825@naver.com>
2019-11-05 16:59:06 +0900

    게시글 작성 route 생성

--- frontend/src/routes/Production/ProductionPresenter.jsx
+++ frontend/src/routes/Production/ProductionPresenter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from 'react-router-dom';
+import Header from 'components/Header';
 import ProduceMenu from "components/ProduceMenu";
 import ProduceTotalList from "components/ProduceTotalList";
 import ProduceMusicList from "components/ProduceMusicList";
 import ProduceSportsList from "components/ProduceSportsList";
 import ProduceMoviesList from "components/ProduceMoviesList";
 import ProduceGamesList from "components/ProduceGamesList";
-import Header from 'components/Header';
+import Write from 'components/Write';
 
 const ProductionPresenter = ({match}) => {
   return (
@@ -18,6 +19,7 @@ const ProductionPresenter = ({match}) => {
       <Route path={`${match.url}/sports`} component={ProduceSportsList}/>
       <Route path={`${match.url}/movies`} component={ProduceMoviesList}/>
       <Route path={`${match.url}/games`} component={ProduceGamesList}/>
+      <Route path={`${match.url}/write`} component={Write}/>
     </>
   );
 };
