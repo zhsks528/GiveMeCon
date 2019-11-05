625afde430306cfe0c267760ed7150aa298b3042
Han Seong Min <hsm950825@naver.com>
2019-11-05 12:12:03 +0900

    Media URL 설정

--- GiveMeCon/urls.py
+++ GiveMeCon/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
 from rest_framework import permissions
+from django.conf.urls.static import static
+from django.conf import settings
 
 # API 문서화
 from drf_yasg.views import get_schema_view
@@ -40,4 +42,4 @@ urlpatterns = [
     url(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
