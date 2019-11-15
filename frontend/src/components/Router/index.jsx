32a2e4e15c76a19d353236e0c4266afb4c580481
zhsks528 <hsm950825@naver.com>
2019-11-15 23:31:58 +0900

    Public / Private Router 구분

--- frontend/src/components/Router/index.jsx
+++ frontend/src/components/Router/index.jsx
@@ -1,16 +1,49 @@
 import React from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Router, Route, Redirect, Switch } from "react-router-dom";
+import { createBrowserHistory as createHistory } from "history";
+
+import Auth from "components/Auth";
+
 import Trend from "routes/Trend";
 import Production from "routes/Production";
 import Introduce from "routes/Introduce";
 
+const history = createHistory();
+
 export default function Routes() {
+  const isLoggedIn = useSelector(state => state.users.isLoggedIn);
+
+  let route = null;
+
+  console.log(isLoggedIn);
+  if (isLoggedIn) {
+    route = <PublicRoutes />;
+  } else {
+    route = <PrivateRoutes />;
+  }
+  return <Router history={history}>{route}</Router>;
+}
+
+export const PrivateRoutes = () => {
   return (
     <Switch>
-      <Route path="/trend" component={Trend} />
+      <Route path="/" component={Auth} />
+      {/* <Route path="/trend" component={Trend} />
+      <Route path="/production" component={Production} />
+      <Route exact path="/introduce" component={Introduce} /> */}
+      <Redirect from="*" to="/" />
+    </Switch>
+  );
+};
+
+export const PublicRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path="/trend" component={Trend} />
       <Route path="/production" component={Production} />
       <Route exact path="/introduce" component={Introduce} />
       <Redirect from="*" to="/trend" />
     </Switch>
   );
-}
+};
