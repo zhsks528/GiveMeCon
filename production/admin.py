4e8440e79238415849eae3a2f9640857d5da019b
zhsks528 <hsm950825@naver.com>
2019-11-15 23:35:58 +0900

    production App 수정

--- production/admin.py
+++ production/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from .models import Production, ProduceMusic, ProduceSports, ProduceMovies, ProduceGame
+from .models import Production, Image, Comment, Like
 
-admin.site.register(Production)
-admin.site.register(ProduceMusic)
-admin.site.register(ProduceSports)
-admin.site.register(ProduceMovies)
-admin.site.register(ProduceGame)
+class ProductionAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'title', 'creator')
+
+admin.site.register(Production, ProductionAdmin)
+
+class ImageAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'post', 'file')
+    
+admin.site.register(Image, ImageAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'creator', 'message', 'created_at', 'updated_at')
+
+admin.site.register(Comment, CommentAdmin)
+
+class LikeAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'creator', 'post')
+
+admin.site.register(Like, LikeAdmin)
+
+
--- production/models.py
+++ production/models.py
@@ -1,36 +1,75 @@
 from django.db import models
 from category.models import Category
+from users import models as user_models
+from category import models as category_models
+from taggit.managers import TaggableManager
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
-
-class Production(models.Model):
-
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    thumnail = models.ImageField(upload_to="images", null=True, blank=True)
-    author = models.CharField(max_length=255)
-    tag = models.CharField(max_length=255, null=True, blank=True)
+class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
+    class Meta:
+        abstract = True
+
+
+class Production(TimeStampedModel):
+
+    """ 게시글 테이블 """
+
+    category = models.ForeignKey(category_models.Category, on_delete=models.CASCADE, null=True, related_name='category')
+    title = models.CharField(max_length=255)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, related_name='production')
+    content = models.TextField()
+    tags = TaggableManager(blank = True)
+
+    class Meta:
+        ordering = ['-created_at']
+        
+    @property
+    def likes_count(self):
+        return self.likes.all().count()
+
+    @property
+    def comments_count(self):
+        return self.comments.all().count()
+
+    @property
+    def natural_time(self):
+        return  naturaltime(self.created_at)
+        
     def __str__(self):
         return self.title
 
-    class Meta:
-        ordering = ["-created_at"]
+
+class Image(TimeStampedModel):
+    post = models.ForeignKey(Production, on_delete=models.CASCADE, null=True, related_name='images')
+    file = models.ImageField(blank=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.post, self.file)
 
 
-class ProduceMusic(Production):
-    pass
+class Comment(TimeStampedModel):
+
+    """ 댓글 테이블 """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    message = models.TextField()
+    post = models.ForeignKey(Production, on_delete=models.CASCADE, null=True, related_name='comments')
+
+    def __str__(self):
+        return '{} - {}'.format(self.creator, self.message)
 
 
-class ProduceSports(Production):
-    pass
+class Like(TimeStampedModel):
+
+    """ 좋아요 테이블 """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Production, on_delete=models.CASCADE, null=True, related_name='likes')
+
+    def __str__(self):
+        return '{} - {}'.format(self.creator, self.post)
 
 
-class ProduceMovies(Production):
-    pass
-
-
-class ProduceGame(Production):
-    pass
