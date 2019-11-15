2474701726e5c6178761759acc4b3d97509850ce
zhsks528 <hsm950825@naver.com>
2019-11-15 23:31:18 +0900

    Registration Components 생성

---
+++ frontend/src/components/Registration/RegistrationContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import RegistrationPresenter from "./RegistrationPresenter";
+import { Registration } from "store/actions/users";
+
+export default function RegistrationContainer({ changeAction }) {
+  const [email, setEmail] = useState("");
+  const [fullname, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(Registration(username, password, email, fullname));
+  };
+
+  return (
+    <RegistrationPresenter
+      changeAction={changeAction}
+      email={email}
+      setEmail={setEmail}
+      fullname={fullname}
+      setFullName={setFullName}
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+    />
+  );
+}
---
+++ frontend/src/components/Registration/RegistrationPresenter.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  border: 2px solid;
+  padding: 20px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 36px;
+`;
+
+const SubTitle = styled.div`
+  font-size: 16px;
+`;
+
+const Body = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ChangeForm = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  border: none;
+  outline: none;
+  background: #f7323f;
+  border-radius: 14px;
+  padding: 5px 10px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  margin-bottom: 20px;
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  && {
+    width: 30px;
+    height: 30px;
+    padding: 10px;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+`;
+
+const Sumbit = styled.input`
+  border: none;
+  border-radius: 14px;
+  background: #15933a;
+  color: white;
+  padding: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  cursor: pointer;
+  outline: none;
+`;
+
+const RegistrationPresenter = ({
+  changeAction,
+  email,
+  setEmail,
+  fullname,
+  setFullName,
+  username,
+  setUsername,
+  password,
+  setPassword,
+  handleSubmit
+}) => {
+  return (
+    <FormContainer>
+      <Header>
+        <Title>회원가입</Title>
+        <SubTitle>기브미콘</SubTitle>
+      </Header>
+      <Body>
+        <Form onSubmit={handleSubmit}>
+          <InputContainer>
+            <Icon icon={faEnvelope} />
+            <Input
+              type="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              placeholder="이메일"
+            />
+          </InputContainer>
+          <InputContainer>
+            <Icon icon={faUser} />
+            <Input
+              type="text"
+              value={fullname}
+              onChange={event => setFullName(event.target.value)}
+              placeholder="성명"
+            />
+          </InputContainer>
+          <InputContainer>
+            <Icon icon={faUser} />
+            <Input
+              type="text"
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+              placeholder="닉네임"
+            />
+          </InputContainer>
+
+          <InputContainer>
+            <Icon icon={faKey} />
+            <Input
+              type="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              placeholder="비밀번호"
+              autocomplete="on"
+            />
+          </InputContainer>
+
+          <Sumbit type="submit" value="회원가입" />
+        </Form>
+      </Body>
+      <ChangeForm>
+        <div>회원이신가요?</div>
+        <Button onClick={changeAction}>로그인</Button>
+      </ChangeForm>
+    </FormContainer>
+  );
+};
+
+export default RegistrationPresenter;
