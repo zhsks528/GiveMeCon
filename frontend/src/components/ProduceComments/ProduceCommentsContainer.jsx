4d850e59e530946c4b9914ee00da3aab026aebcd
zhsks528 <hsm950825@naver.com>
2019-11-16 23:59:09 +0900

    ProduceComments commentDelete 추가

--- frontend/src/components/ProduceComments/ProduceCommentsContainer.jsx
+++ frontend/src/components/ProduceComments/ProduceCommentsContainer.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import ProduceCommentsPresenter from "./ProduceCommentsPresenter";
+import { commentDelete } from "store/actions/production";
 
 export default function ProduceCommentsContainer({ comments }) {
-  return <ProduceCommentsPresenter comments={comments} />;
+  const dispatch = useDispatch();
+
+  const handleDelete = commentId => {
+    dispatch(commentDelete(commentId));
+  };
+  return (
+    <ProduceCommentsPresenter comments={comments} handleDelete={handleDelete} />
+  );
 }
--- frontend/src/components/ProduceComments/ProduceCommentsPresenter.jsx
+++ frontend/src/components/ProduceComments/ProduceCommentsPresenter.jsx
@@ -1,18 +1,82 @@
 import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
-const ProduceCommentsPresenter = ({ comments }) => {
+const ListContainer = styled.ul`
+  padding: 0;
+`;
+
+const List = styled.li`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  padding: 0 20px;
+`;
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Profile = styled.img`
+  width: 40px !important;
+  height: 40px !important;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const NoProfile = styled(FontAwesomeIcon)`
+  && {
+    width: 40px !important;
+    height: 40px !important;
+    border-radius: 50%;
+    margin-right: 10px;
+    color: #6b6b6b;
+  }
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 24px;
+  color: #a9a9a9;
+  cursor: pointer;
+  margin-left: 10px;
+
+  &:hover {
+    color: #f7323f;
+  }
+`;
+
+const Message = styled.span`
+  word-break: break-all;
+`;
+const ProduceCommentsPresenter = ({ comments, handleDelete }) => {
   return (
-    <div>
-      <ul>
-        {comments.map(comment => (
-          <li key={comment.id}>
-            <span>
-              {comment.creator.username} {comment.message}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <ListContainer>
+        {comments
+          ? comments.map(comment => (
+              <List key={comment.id}>
+                <UserContainer>
+                  {comment.creator.profile_image ? (
+                    <Profile src={comment.creator.profile_image} />
+                  ) : (
+                    <NoProfile icon={faUserCircle} />
+                  )}
+
+                  <div>
+                    <div>{comment.creator.username}</div>
+                    <Message>{comment.message}</Message>
+                  </div>
+                </UserContainer>
+                <Icon onClick={() => handleDelete(comment.id)} icon={faTimes} />
+              </List>
+            ))
+          : null}
+      </ListContainer>
+    </>
   );
 };
 
