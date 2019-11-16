9bef0a570bf3bdcc60025f13d7a4a43c328b907a
zhsks528 <hsm950825@naver.com>
2019-11-16 23:25:18 +0900

    CommentBox resize css 적용

--- frontend/src/components/Comments/CommentsPresenter.jsx
+++ frontend/src/components/Comments/CommentsPresenter.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import TextareaAutosize from "react-textarea-autosize";
 
+const TextWrapper = styled.form`
+  padding: 14px 0 14px 10px;
+`;
+const TextArea = styled(TextareaAutosize)`
+  && {
+    border: none;
+    height: 23px;
+    resize: none;
+    width: 100%;
+    font-size: 20px;
+    outline: none;
+  }
+`;
 const CommentsPresenter = ({ message, setMessage, handleKeyPress }) => {
   return (
-    <form>
-      <TextareaAutosize
+    <TextWrapper>
+      <TextArea
         value={message}
         onChange={event => setMessage(event.target.value)}
         onKeyPress={handleKeyPress}
-        placeholder="댓글을 입력해주세요"
+        placeholder="댓글 달기..."
       />
-    </form>
+    </TextWrapper>
   );
 };
 
