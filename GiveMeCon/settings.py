c5f366c6734e866f7f7ee1e53ab45e69f58a8e77
ji-hae <51120502+YOONJIHAE@users.noreply.github.com>
2019-11-17 03:37:16 +0900

    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')

--- GiveMeCon/settings.py
+++ GiveMeCon/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # 페이지네이션 추가
