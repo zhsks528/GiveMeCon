ee2ad8e689e3aaf56182695304cc1db2621ca49f
ji-hae <51120502+YOONJIHAE@users.noreply.github.com>
2019-11-18 22:50:47 +0900

    Update serializers.py

--- video/serializers.py
+++ video/serializers.py
@@ -4,4 +4,4 @@ from .models import Video
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = '__all__'
+        fields = ("id", 'title', "view", 'channel', 'thumbnail', 'created_at', 'updated_at', 'video_num', 'topic', 'category', 'url', 'natural_time')
