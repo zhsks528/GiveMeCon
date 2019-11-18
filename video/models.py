0edd53c152cecc70cf58d5b98c69a42073b03b58
zhsks528 <hsm950825@naver.com>
2019-11-19 03:06:45 +0900

    Video Model natural_time 속성 추가

--- video/models.py
+++ video/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from channel.models import Channel
 from category.models import Category
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class Video(models.Model):
     
@@ -17,3 +18,7 @@ class Video(models.Model):
     
     def __str__(self):
         return self.title
+
+    @property
+    def natural_time(self):
+        return  naturaltime(self.created_at)
