f28d2937fe28a5fea2e48b22e21e9be14a83f216
ji-hae <51120502+YOONJIHAE@users.noreply.github.com>
2019-11-24 04:02:41 +0900

    채널 섬네일 추가

--- channel/models.py
+++ channel/models.py
@@ -6,10 +6,10 @@ class Channel(models.Model):
     channel_id = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=30)
     keyword = models.CharField(max_length=30, null=True, blank=True)
-    url = models.CharField(max_length=255, null=True, blank=True)
     subscribers = models.IntegerField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     url = models.CharField(max_length=255, null=True, blank=True, default='https://www.youtube.com/channel/')
-
+    thumbnail = models.CharField(max_length=255, null=True, blank=True)
+    
     def __str__(self):
         return self.name 
