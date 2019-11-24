048427eeba40fde39ddece96fc1bd24861181b20
SeongMin Han <hsm950825@naver.com>
2019-11-24 20:00:49 +0900

    카테고리 리스트 수정

--- frontend/src/components/SelectCategory/SelectCategoryPresenter.jsx
+++ frontend/src/components/SelectCategory/SelectCategoryPresenter.jsx
@@ -56,13 +56,31 @@ const SelectCategoryPresenter = ({
           onChange={event => setCategoryId(event.target.value)}
         >
           <option value={"0"}>카테고리 검색</option>
-          {categoryList.map(item => (
+          <option value={"1"}>영화 & 애니메이션</option>
+          <option value={"2"}>자동차 & 오토바이</option>
+          <option value={"10"}>음악</option>
+          <option value={"15"}>애완동물 & 동물</option>
+          <option value={"17"}>운동</option>
+          <option value={"19"}>여행 & 이벤트</option>
+          <option value={"20"}>게임</option>
+          <option value={"21"}>브이로그</option>
+          <option value={"22"}>사람 & 블로그</option>
+          <option value={"23"}>코미디</option>
+          <option value={"24"}>예능</option>
+          <option value={"25"}>뉴스 & 시사</option>
+          <option value={"26"}>Howto & Style</option>
+          <option value={"27"}>교육</option>
+          <option value={"28"}>과학 & 기술</option>
+          <option value={"29"}>Nonprofits & Activism</option>
+          <option value={"35"}>다큐멘터리</option>
+
+          {/* {categoryList.map(item => (
             <>
               <option key={item.category_id} value={item.category_id}>
                 {item.category_name}
               </option>
             </>
-          ))}
+          ))} */}
         </Select>
         <Btn onClick={handleReset}>
           <FontAwesomeIcon icon={faUndo} />
