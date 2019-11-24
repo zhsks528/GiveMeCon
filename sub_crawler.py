d22365952a9265036a42c0af2d141e8ef284c3ad
ji-hae <51120502+YOONJIHAE@users.noreply.github.com>
2019-11-24 16:10:31 +0900

    채널 섬네일 인덱스에러 수정

--- sub_crawler.py
+++ sub_crawler.py
@@ -53,12 +53,17 @@ def Video_info(video_id):
     
     # for topic in video_data["items"][0]["topicDetails"]["relevantTopicIds"][0].split(','):
     #     print(check_topic(topic))
-    thumbnail=channel_data["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+    #thumbnail=channel_data["items"][0]["snippet"]["thumbnails"]["high"]["url"]
     try:
         subscribeCount = channel_data["items"][0]["statistics"]["subscriberCount"]
     except IndexError:
         subscribeCount = 0
     
+    try:
+        thumbnail=channel_data["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+    except IndexError:
+        thumbnail = 0
+        
     print("채널 구독자수: ", subscribeCount)
 
     # # 채널을 만든다.
